503482e01ea9e43b304ba25128b7de63d8f377a1
amkurian <amkurian@gmail.com>
2019-01-22 15:04:54 +0530

    Added articles model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,11 @@ class ArticlesController < ApplicationController
   def new
   end
 
+
   def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
   end
 
   def show
@@ -18,5 +22,10 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
